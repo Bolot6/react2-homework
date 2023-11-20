edce10589a29fe2d728ca828bc6815e188a22463
bolot6 <kurmanaliev78@gmail.com>
2023-11-20 16:15:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+  const [input1, setInput1] = useState();
+  const [input2, setInput2] = useState();
+  const [result, setResult] = useState();
+
+  const handleInput1Change = (e) => {
+    setInput1(Number(e.target.value));
+  };
+
+  const handleInput2Change = (e) => {
+    setInput2(Number(e.target.value));
+  };
+
+  const handlePlus = () => {
+    setResult(input1 + input2);
+  };
+
+  const handleMinus = () => {
+    setResult(input1 - input2);
+  };
+
+  const handleMultiply = () => {
+    setResult(input1 * input2);
+  };
+
+  const handleDivide = () => {
+    if (input2 !== 0) {
+      setResult(input1 / input2);
+    } else {
+      alert("Are you dumb? You can't divide by zero");
+    }
+  };
+
+  const handleReset = () => {
+    setInput1(0);
+    setInput2(0);
+    setResult(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Calculator</h2>
+      <label>
+        Input 1:
+        <input type="number" value={input1} onChange={handleInput1Change} />
+      </label>
+      <label>
+        Input 2:
+        <input type="number" value={input2} onChange={handleInput2Change} />
+      </label>
+      <button onClick={handlePlus}>Plus</button>
+      <button onClick={handleMinus}>Minus</button>
+      <button onClick={handleMultiply}>Multiply</button>
+      <button onClick={handleDivide}>Divide</button>
+      <button onClick={handleReset}>Reset</button>
+      <h2>Result: {result}</h2>
     </div>
   );
 }
